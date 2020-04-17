25fb1dac8b66d4e4299643334d459c8c79a86b74
margaeor <margaeor@gmail.com>
2020-04-17 20:58:42 +0300

    Initiate auth api

--- auth-db/db_init/1_schema.sql
+++ auth-db/db_init/1_schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS `users` (
     `id` int(11) NOT NULL auto_increment,
-    `username` varchar(32) NOT NULL,
-    `password` varchar(50) NOT NULL,
-    `email` varchar(64) NOT NULL,
+    `username` varchar(32) NOT NULL UNIQUE,
+    `password` varchar(64) NOT NULL ,
+    `email` varchar(64) NOT NULL UNIQUE,
     `role` varchar(16) NOT NULL,
-    `last_login` int(10) NOT NULL,
+    `last_login` DATETIME,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `access_tokens` (
-	`id` int(32) NOT NULL,
+	`id` varchar(64) NOT NULL,
     `user_id` int(11) NOT NULL,
     `date_created` DATETIME NOT NULL,
     `valid_until` DATETIME,
