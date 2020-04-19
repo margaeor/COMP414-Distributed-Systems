c7dfbff88b43864c3ae03b7fa33390ebfa0992d0
antheas <antheas@users.noreply.github.com>
2020-04-19 00:11:37 +0000

    Add configs

---
+++ web/webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  // Entry / Output
+  entry: "./src/App.tsx",
+  output: {
+    filename: "bundle.[hash:8].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 1234,
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    // Setup template
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      minify: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        // Javascript Files
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        // CSS Files / Styling
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        // Image Support
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "./img/[name].[contenthash:8].[ext]" }
+        }
+      },
+      {
+        // Fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "./res/[name].[contenthash:8].[ext]" }
+        }
+      }
+    ]
+  }
+};
+
+if (process.env.NODE_ENV === "development") {
+  module.exports.mode = "development";
+  module.exports.devtool = "inline-source-map";
+  module.exports.optimization = {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false
+  };
+  module.exports.output.pathinfo = false;
+} else {
+  module.exports.mode = "production";
+  module.exports.optimization = {
+    splitChunks: {
+      chunks: "async"
+    }
+  };
+}
