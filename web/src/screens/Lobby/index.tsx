1bdf1a7f86cbb89b04c77f8bd65de366d580233e
antheas <antheas@users.noreply.github.com>
2020-04-21 01:32:23 +0000

    Add lobby stub

--- web/src/screens/Lobby/index.tsx
+++ web/src/screens/Lobby/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./style.css";
 
 const Login: React.FunctionComponent = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="quickPlay">
+        <span>Quick Play</span>
+        <button>Chess</button>
+        <button>Tic Tac Toe</button>
+      </div>
+      <div className="tournamentPlay">
+        <span>Tournaments</span>
+        <ul id="tournamentList"></ul>
+      </div>
+      <div className="currentPlays">
+        <span>Ongoing Matches</span>
+        <ul id="ongoingPlays"></ul>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
