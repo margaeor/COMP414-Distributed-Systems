98a4dbbe92c5c35c5111593879884bd07ddd79db
George Margaritis <margaeor@gmail.com>
2020-04-30 02:56:23 +0300

    Try to merge docker compose

--- api/mongo/init/init_mongo.sh
+++ api/mongo/init/init_mongo.sh
@@ -5,7 +5,7 @@ export MONGODB_API_PASS=$(cat /run/secrets/mongodb_api_password)
 
 
 # Asynchronous sleep and add users to wait for db initialization
-bash -c 'sleep 20 && mongo -u root -p ${MONGODB_ROOT_PASSWORD} <<EOF
+bash -c 'sleep 30 && mongo -u root -p ${MONGODB_ROOT_PASSWORD} <<EOF
 use ${MONGODB_API_DB}
 db.createUser({user: "${MONGODB_API_USER}",pwd:"${MONGODB_API_PASS}",roles:[{role:"readWrite",db:"${MONGODB_API_DB}"}]})
 EOF' &
