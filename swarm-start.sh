ec619ae6768c01f4bc1ad361693f90f47c34e843
antheas <antheas@users.noreply.github.com>
2020-06-23 19:51:07 +0000

    make small change to start script

--- swarm-start.sh
+++ swarm-start.sh
@@ -3,5 +3,6 @@
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 docker-compose -f docker-compose-swarm.yml build
 docker-compose -f docker-compose-swarm.yml push
-docker-compose -f docker-compose-swarm.yml config > docker-compose-expanded.yml && docker stack deploy --compose-file docker-compose-expanded.yml comp414
+docker-compose -f docker-compose-swarm.yml config > docker-compose-expanded.yml
+docker stack deploy --compose-file docker-compose-expanded.yml comp414
 
